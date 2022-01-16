dadfda44625f118d092b2631eae7ed51eb1180f5
Avinash Bawane <avinashbawane11@gmail.com>
2022-01-15 18:51:08 -0700

    Compeleted HTML & CSS part of the game

---
+++ rock-paper-scissors/app.js
@@ -0,0 +1,11 @@
+// console.log("Hello, this is Avinash Bawane");
+
+// first, cache the DOM
+
+const userScore = 0;
+
+const computerScore = 0;
+
+const userScore_span = document.getElementById("user-score");
+
+const userScore_span = document.getElementById("user-score");
